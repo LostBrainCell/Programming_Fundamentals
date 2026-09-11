b0689311fdfea0f5d3f55379d6dc1a4a5f456c82
LostBrainCell <kadrvex@gmail.com>
2026-09-11 10:55:54 -0600

    moved files around

---
+++ lessons/lesson 4/conditionals.py
@@ -0,0 +1,15 @@
+
+username = input("Luke")
+password = input("password?")
+
+if username != "Luke" and password == "1234":
+    print("Logged in")
+else:
+    print("Invalid credentials")
+
+# Equals = =
+# Not equals = !=
+# Greater than = >
+# Less than = <
+# Greater than or equal to = >=
+# Less than or equal to = <=
